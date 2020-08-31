c30b3ed4660e7c8ee17c4c5cb8ebf85870590610
array <annaraykalashetty1999@gmail.com>
2020-08-31 14:26:59 -0700

    Replaced Login with cookie
    cookie_file variable has locally stored cookies

--- wdriver.py
+++ wdriver.py
@@ -1,5 +1,5 @@
 import time
-
+import pickle
 from selenium import webdriver
 from selenium.webdriver.chrome import service
 from bs4 import BeautifulSoup as bss
@@ -9,15 +9,16 @@ webdriver_service.start()
 
 driver = webdriver.Remote(webdriver_service.service_url, webdriver.DesiredCapabilities.OPERA)
 
-driver.get('https://instagram.com/accounts/login')
-time.sleep(2)
-userText = driver.find_element_by_name("username")
-userText.send_keys('hack_it_like_you_know\t')
-input("this is the time you enter password and press enter ")
-notnow = driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/div/div/div/button""")
-notnow.click()
-driver.find_element_by_xpath("""/html/body/div[4]/div/div/div/div[3]/button[2]""").click()
-driver.get("https://www.instagram.com/hack_it_like_you_know/followers")
+myUserId = 'hack_it_like_you_know'
+driver.get('https://instagram.com/')
+cookie_file='cookie.data'
+cookies = pickle.load(open(cookie_file, "rb"))
+for i in cookies:
+	driver.add_cookie(i)
+driver.get('https://instagram.com/'+myUserId+'/followers')
 driver.find_element_by_xpath("""//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a""").click()
+
 followersPageSource = driver.page_source
 soup = bss(followersPageSource, features='html.parser')
+
+time.sleep(10)
